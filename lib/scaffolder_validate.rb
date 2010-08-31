f409c84bcf212605adab3505387e6aaf0a825e0c
Michael Barton <mail@michaelbarton.me.uk>
2010-08-31 14:26:57 -0400

    Method to compare inserts for overlaps

--- lib/scaffolder_validate.rb
+++ lib/scaffolder_validate.rb
@@ -8,4 +8,8 @@ class ScaffolderValidate
     @scaffold.select{|i| ! i.valid? }
   end
 
+  def self.inserts_overlap?(a,b)
+    ! (a.position.to_a & b.position.to_a).empty?
+  end
+
 end
--- spec/scaffold_validate_spec.rb
+++ spec/scaffold_validate_spec.rb
@@ -2,6 +2,26 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 context ScaffolderValidate do
 
+  context "comparing inserts for overlaps using inserts_overlap?" do
+
+    before(:all) do
+      @insert = stub(:position => 5..10)
+    end
+
+    it "should be false when inserts don't overlap" do
+      no_overlap = stub(:position => 11..12)
+      ScaffolderValidate.inserts_overlap?(@insert,no_overlap).should be_false
+      ScaffolderValidate.inserts_overlap?(no_overlap,@insert).should be_false
+    end
+
+    it "should be true when inserts do overlap" do
+      overlap = stub(:position => 10..11)
+      ScaffolderValidate.inserts_overlap?(@insert,overlap).should be_true
+      ScaffolderValidate.inserts_overlap?(overlap,@insert).should be_true
+    end
+
+  end
+
   context "calling the errors method" do
     before(:all) do
       @valid   = stub(:valid? => true)
